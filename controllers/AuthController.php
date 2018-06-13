be0cdf54562eef8ed4484d0f14a49be8c75f7c9f
Ulysse Hall <ulysse.hall@gmail.com>
2018-06-13 11:39:48 +0200

    Connexion -> ok

---
+++ controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Controllers;
+
+class AuthController {
+
+    function loginAction($twig, $bdd) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $req = $bdd->prepare("SELECT username FROM user WHERE username = :username AND password = :password");
+            $req->execute([
+                'username' => $_POST['username'],
+                'password' => md5($_POST['password'])
+            ]);
+
+            if($req->fetch()) {
+                $_SESSION['connected'] = true;
+                header('Location: ?action=home');
+            } else {
+                header('Location: ?action=login');
+            }
+
+        } else {
+            echo $twig->render('Auth/login.html');
+        }
+    }
+
+    function registerAction($twig, $bdd) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $req = $bdd->prepare("SELECT username FROM user WHERE username = :username");
+            $req->execute(['username' => $_POST['username']]);
+
+            if($req->fetch()) {
+                echo $twig->render('Auth/register.html', ['error' => 'Username already taken']);
+                return false;
+            }
+
+            $req = $bdd->prepare("SELECT username FROM user WHERE email = :email");
+            $req->execute(['email' => $_POST['email']]);
+
+            if($req->fetch()) {
+                echo $twig->render('Auth/register.html', ['error' => 'Email already taken']);
+                return false;
+            }
+
+            $req = $bdd->prepare("INSERT INTO user(email, username, password) VALUES (:email, :username, :password)");
+            $req->execute([
+                'email' => $_POST['email'],
+                'username' => $_POST['username'],
+                'password' => md5($_POST['password'])
+            ]);
+
+            $_SESSION['connected'] = true;
+            header('Location: ?action=home');
+        } else {
+            echo $twig->render('Auth/register.html');
+        }
+    }
+
+}
